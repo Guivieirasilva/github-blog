bf60921b8a95666c1d9008dcafc91def9e84ece9
gui vieira <guivieirasilva02@gmail.com>
2024-02-28 00:21:24 -0300

    Estrutura e estilização do Header Principal

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export function Header() {
+  return (
+    <header className="flex flex-col items-center justify-center ">
+      <img src="/cover.png" alt="" className="h-72 w-full" />
+      <img
+        src="/Logo.svg"
+        alt=""
+        className="mx-auto mb-10 mt-[-250px] max-w-40"
+      />{' '}
+    </header>
+  )
+}
