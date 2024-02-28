06c56eca2d1b802393ed9969dd6066e5b38d1c1c
gui vieira <guivieirasilva02@gmail.com>
2024-02-28 00:35:09 -0300

    adicionando retorno e tratamento de informações do contexto de issues

--- src/pages/Blog/components/PostCard/index.tsx
+++ src/pages/Blog/components/PostCard/index.tsx
@@ -1,24 +1,44 @@
 import { useNavigate } from 'react-router-dom'
+import { format, formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 
-export function PostCard() {
+interface PostCardProps {
+  id: string
+  title: string
+  description: string
+  createdAt: string
+}
+
+export function PostCard({ id, title, description, createdAt }: PostCardProps) {
   const nagivate = useNavigate()
+
+  const publishedDateFormated = format(createdAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(createdAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+
+  const shortDescription = description.substring(0, 250).concat('...')
+
   return (
     <div
-      onClick={() => nagivate('posts/2')}
+      onClick={() => nagivate(`posts/${id}`)}
       className="cursor-pointer rounded-[10px] bg-base-post p-10"
     >
       <header className="mb-4 flex items-center justify-between gap-1">
-        <strong className="text-xl text-base-title">
-          JavaScript data types and data structures
-        </strong>
-        <span className="text-sm  text-base-span">Há 1 dia</span>
+        <strong className="text-xl text-base-title">{title}</strong>
+        <time
+          title={publishedDateFormated}
+          dateTime={createdAt}
+          className="text-sm  text-base-span"
+        >
+          {publishedDateRelativeToNow}
+        </time>
       </header>
-      <article>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad beatae
-        voluptas earum alias exercitationem libero facilis porro sed officiis
-        recusandae soluta inventore incidunt, natus laudantium perspiciatis
-        repellat vitae facere odio!
-      </article>
+      <article>{shortDescription}</article>
     </div>
   )
 }
--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,8 +1,14 @@
+import { useContext } from 'react'
 import { PostCard } from './components/PostCard'
 import { ProfileCard } from './components/ProfileCard'
+import { IssuesContext } from '../../context/IssuesContext'
 
 export function Blog() {
-  const POSTS = [1, 2, 3, 4, 5, 6]
+  const { issues } = useContext(IssuesContext)
+  const amountIssues =
+    issues.length === 1
+      ? `${issues.length} Publicação`
+      : `${issues.length} publicações`
 
   return (
     <div className="flex flex-col items-center justify-center gap-16 pb-40 text-base-text ">
@@ -10,17 +16,27 @@ export function Blog() {
       <div className="flex w-4/5 flex-col items-center justify-center gap-2 md:w-[90%]">
         <div className="flex w-full justify-between px-1">
           <span className="text-lg text-base-subtitle ">Publicações</span>
-          <span className="text-sm text-base-span ">9 Publicações</span>
+          <span className="text-sm text-base-span ">
+            {issues.length >= 1 && amountIssues}
+          </span>
         </div>
+
         <input
           className="h-12 w-full rounded-md border border-base-border bg-base-input p-4 text-base-text placeholder:text-base-label "
           type="search"
           placeholder="Buscar Conteúdo"
         />
       </div>
+
       <section className=" grid w-4/5 grid-cols-2 gap-8 md:w-[90%] md:grid-cols-1">
-        {POSTS.map((data, idx) => (
-          <PostCard key={idx} />
+        {issues.map((data) => (
+          <PostCard
+            key={data.id}
+            id={data.id}
+            title={data.title}
+            description={data.body}
+            createdAt={data.created_at}
+          />
         ))}
       </section>
     </div>
