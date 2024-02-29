807d5844989c98ff0f2206eadb5c75c91fc9eee2
gui vieira <guivieirasilva02@gmail.com>
2024-02-29 11:34:37 -0300

    retorno de informações do contexto de perfil

--- src/pages/Blog/components/ProfileCard/index.tsx
+++ src/pages/Blog/components/ProfileCard/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import {
   faUpRightFromSquare,
   faBuilding,
@@ -5,8 +6,13 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import IconGithub from '../../../../assets/icons/github.svg'
+import { useContext } from 'react'
+import { ProfileContext } from '../../../../context/ProfileContext'
+import Markdown from 'react-markdown'
 
 export function ProfileCard() {
+  const { user } = useContext(ProfileContext)
+  const { bio, company, followers, html_url, login, name } = user
   return (
     <section className="flex w-4/5 gap-4 rounded-lg bg-base-profile p-10 text-base-text md:w-[90%] md:flex-col md:items-center md:p-6 lg:flex-row">
       <div className="flex items-center">
@@ -19,33 +25,28 @@ export function ProfileCard() {
       <div className=" flex flex-1 flex-col gap-4 p-2">
         <header className="flex items-center justify-between md:flex-col md:gap-2">
           <strong className="text-2xl text-base-title md:text-center ">
-            Guilherme Vieira Silva
+            {name}
           </strong>
-          <a className="cursor-pointer text-blue">
+          <a href={html_url} className="cursor-pointer text-blue">
             GITHUB{' '}
             <FontAwesomeIcon className="ml-1" icon={faUpRightFromSquare} />
           </a>
         </header>
-        <main>
-          <p className="md:text-justify">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus
-            nesciunt cumque tempora animi deleniti exercitationem est, aliquam
-            aut iusto ab, nostrum minus sit, architecto ipsa necessitatibus
-            perferendis eaque sequi cum.
-          </p>
+        <main className="flex-1">
+          <Markdown>{bio}</Markdown>
         </main>
         <footer className="flex justify-between gap-4 text-center text-base-subtitle md:flex-col md:items-center">
           <div title="Github" className="flex items-center gap-2">
             <img className="inline h-6 w-6" src={IconGithub} alt="" />
-            <span>GuivieiraSilva</span>
+            <span>{login}</span>
           </div>
           <div title="Empresa" className="flex items-center gap-2">
             <FontAwesomeIcon icon={faBuilding} />
-            UpBase Consulthing
+            {company}
           </div>
           <div title="Seguidores" className="flex items-center gap-2">
             <FontAwesomeIcon icon={faUserGroup} />
-            32 Seguidores
+            {followers} Seguidores
           </div>
         </footer>
       </div>
