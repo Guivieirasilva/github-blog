b14a8ba822ef2db41adef8db1edde843e481e87f
gui vieira <guivieirasilva02@gmail.com>
2024-03-06 00:34:14 -0300

    form de busca | mensagem de issues não encontradas

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,14 +1,22 @@
-import { useContext } from 'react'
+import { FormEvent, useContext, useState } from 'react'
 import { PostCard } from './components/PostCard'
 import { ProfileCard } from './components/ProfileCard'
 import { IssuesContext } from '../../context/IssuesContext'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export function Blog() {
-  const { issues } = useContext(IssuesContext)
+  const [search, setSearch] = useState<string>('')
+  const { issues, fetchUserIssues } = useContext(IssuesContext)
   const amountIssues =
     issues.length === 1
       ? `${issues.length} Publicação`
       : `${issues.length} publicações`
+
+  function handleSearchIssues(e: FormEvent) {
+    e.preventDefault()
+    fetchUserIssues(search)
+  }
 
   return (
     <div className="flex flex-col items-center justify-center gap-16 pb-40 text-base-text ">
@@ -21,12 +29,28 @@ export function Blog() {
           </span>
         </div>
 
-        <input
-          className="h-12 w-full rounded-md border border-base-border bg-base-input p-4 text-base-text placeholder:text-base-label "
-          type="search"
-          placeholder="Buscar Conteúdo"
-        />
+        <form onSubmit={handleSearchIssues} className="flex w-full gap-2">
+          <input
+            className="h-12 w-full rounded-md border border-base-border bg-base-input p-4 text-base-text placeholder:text-base-label "
+            type="search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Buscar Conteúdo"
+          />
+          <button
+            type="submit"
+            className="w-12 rounded-md border border-base-border"
+          >
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </form>
       </div>
+
+      {issues.length === 0 && (
+        <div className="p-10">
+          <strong className="text-xl">Não há Issues para Exibir!</strong>
+        </div>
+      )}
 
       <section className=" grid w-4/5 grid-cols-2 gap-8 md:w-[90%] md:grid-cols-1">
         {issues.map((data) => (
